6378cbfc0cd18a622565d338c33ba840d7614898
Tom Ellis <elthma@amazon.com>
2019-10-17 16:31:20 +1100

    added final linke

--- document-linker.js
+++ document-linker.js
@@ -14,15 +14,13 @@
 (function () {
   'use strict';
 
-  var TitleFirst = "<button class=\"btn-copy-to-clipboard\" type=\"button\">[ ] Title - Link</button>";
-  var LinkFirst = "<button class=\"btn-copy-to-clipboard\" type=\"button\">[ ] Link - Title</button>";
-
   $("a.anchorjs-link")
     .after("<div class=\"buttonDiv\"></div>")
 
   $("div.buttonDiv")
     .append("<button class=\"PlainLink btn-copy-to-clipboard\" type=\"button\">Plain link</button>")
-    .append("<button class=\"TitleLink btn-copy-to-clipboard\" type=\"button\">[ ] Title - Link</button>");
+    .append("<button class=\"TitleFirst btn-copy-to-clipboard\" type=\"button\">[ ] Title - Link</button>")
+    .append("<button class=\"LinkFirst btn-copy-to-clipboard\" type=\"button\">[ ] Link - Title</button>");
 
    $(".PlainLink").click(function(){
       var anchor = $(this).parents(".buttonDiv").prev();
@@ -34,7 +32,7 @@
       GM_setClipboard(url);
   });
 
-    $(".TitleLink").click(function(){
+    $(".TitleFirst").click(function(){
       var anchor = $(this).parents(".buttonDiv").prev();
       var origin = $(anchor)[0].origin;
       var path = $(anchor)[0].pathname;
@@ -44,32 +42,39 @@
       var title= htmlTitle.substr(0, htmlTitle.indexOf('<a')); 
       var url = origin + path + hash;
       var formatted = "[] " + title + " - " + url;
-            
+      
       GM_setClipboard(formatted);
       
   });
 
+  $(".LinkFirst").click(function(){
+    var anchor = $(this).parents(".buttonDiv").prev();
+    var origin = $(anchor)[0].origin;
+    var path = $(anchor)[0].pathname;
+    var hash = $(anchor)[0].hash;
+    var html = $(this).parents(".buttonDiv").prev().parent()
+    var htmlTitle = $(html)[0].innerHTML
+    var title= htmlTitle.substr(0, htmlTitle.indexOf('<a')); 
+    var url = origin + path + hash;
+    var formatted = "[] " + url + " - " + title;
+    
+    GM_setClipboard(formatted);
+    
+});
+
 })();
-
-
-
-
-
-
-
-
 
 GM_addStyle(`
   .btn-copy-to-clipboard {
-background-color: #326CE6; /* Green */
-border: none;
-color: white;
-padding: 5px 8px 5px 8px;
-margin: 2px 10px 2px 0px;
-text-align: center;
-text-decoration: none;
-display: inline-block;
-font-size: 16px;
-border-radius: 5px;
-}
+    background-color: #326CE6; /* Green */
+    border: none;
+    color: white;
+    padding: 5px 8px 5px 8px;
+    margin: 2px 10px 2px 0px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 5px;
+  }
 `);
