4662b5d2aa1d7fc8187a4b50d886d597f62d028b
tde908 <tomellis91@gmail.com>
2019-10-17 21:23:15 +1100

    Update document-linker.js

--- document-linker.js
+++ document-linker.js
@@ -3,13 +3,15 @@
 // @namespace    https://kubernetes.io
 // @include      https://kubernetes.io/*
 // @version      0.1
-// @description  Dynamically creates buttons to copy a formatted link for use in customer correspondences.
+// @description  Dynamically add buttons under anchor links, format and copy them for use in customer correspondences.
 // @author       Tom Ellis
 // @match        https://kubernetes.io/*
 // @grant        GM_addStyle
 // @grant        GM_setClipboard
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // ==/UserScript==
+
+var $ = window.jQuery;
 
 (function () {
   'use strict';
@@ -22,47 +24,45 @@
     .append("<button class=\"TitleFirst btn-copy-to-clipboard\" type=\"button\">[ ] Title - Link</button>")
     .append("<button class=\"LinkFirst btn-copy-to-clipboard\" type=\"button\">[ ] Link - Title</button>");
 
-   $(".PlainLink").click(function(){
+  $(".PlainLink").click(function () {
     var elements = GetElements(this);
-    
+
     GM_setClipboard(elements.URL);
   });
 
-    $(".TitleFirst").click(function(){
-      var elements = GetElements(this);
-      var formatted = "[] " + elements.Title + " - " + elements.URL;
-      
-      GM_setClipboard(formatted);
-      
+  $(".TitleFirst").click(function () {
+    var elements = GetElements(this);
+    var formatted = "[] " + elements.Title + " - " + elements.URL;
+
+    GM_setClipboard(formatted);
   });
 
 
-$(".LinkFirst").click(function(){
+  $(".LinkFirst").click(function () {
     var elements = GetElements(this);
     var formatted = "[] " + elements.URL + " - " + elements.Title;
-    
-    GM_setClipboard(formatted);
-    
-  });
 
+    GM_setClipboard(formatted);
+  });
 
 })();
 
-function GetElements(el){
-    var anchor = $(el).parents(".buttonDiv").prev();
-    var origin = $(anchor)[0].origin;
-    var path = $(anchor)[0].pathname;
-    var hash = $(anchor)[0].hash;
-    var html = $(el).parents(".buttonDiv").prev().parent()
-    var htmlTitle = $(html)[0].innerHTML
-    var title= htmlTitle.substr(0, htmlTitle.indexOf('<a')); 
-    var url = origin + path + hash;
-    return {"Title": title, "URL": url}
+function GetElements(el) {
+  var anchor = $(el).parents(".buttonDiv").prev();
+  var origin = $(anchor)[0].origin;
+  var path = $(anchor)[0].pathname;
+  var hash = $(anchor)[0].hash;
+  var html = $(el).parents(".buttonDiv").prev().parent()
+  var htmlTitle = $(html)[0].innerHTML
+  var title = htmlTitle.substr(0, htmlTitle.indexOf('<a'));
+  var url = origin + path + hash;
+
+  return { "Title": title, "URL": url }
 }
 
 GM_addStyle(`
   .btn-copy-to-clipboard {
-    background-color: #326CE6; /* Green */
+    background-color: #326CE6;
     border: none;
     color: white;
     padding: 5px 8px 5px 8px;
