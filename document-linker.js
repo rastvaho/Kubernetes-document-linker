d8ff58cd74e34af7ab9dc89e8ea0ba4decfddf3d
tomellis91@gmail.com <tomellis91@gmail.com>
2019-11-12 21:50:35 +1100

    feature(snackbar): Styled buttons and added snackbar functionality

--- document-linker.js
+++ document-linker.js
@@ -18,31 +18,35 @@ var $ = window.jQuery;
 
   $("a.anchorjs-link")
     .after("<div class=\"buttonDiv\"></div>")
+    
 
   $("div.buttonDiv")
-    .append("<button class=\"PlainLink btn-copy-to-clipboard\" type=\"button\">Plain link</button>")
-    .append("<button class=\"TitleFirst btn-copy-to-clipboard\" type=\"button\">[ ] Title - Link</button>")
-    .append("<button class=\"LinkFirst btn-copy-to-clipboard\" type=\"button\">[ ] Link - Title</button>");
+    .append("<button class=\"PlainLink btn btn-primary\" type=\"button\">Plain link</button>")
+    .append("<button class=\"LinkFirst btn btn-primary\" type=\"button\">[ ] Link - Title</button>")
+    .append("<button class=\"TitleFirst btn btn-primary\" type=\"button\">[ ] Title - Link</button>")
+    .append("<div id=\"snackbar\">Copied link!</div>");
 
   $(".PlainLink").click(function () {
     var elements = GetElements(this);
-
+    
     GM_setClipboard(elements.URL);
+    showSnackBar();
   });
-
-  $(".TitleFirst").click(function () {
-    var elements = GetElements(this);
-    var formatted = "[] " + elements.Title + " - " + elements.URL;
-
-    GM_setClipboard(formatted);
-  });
-
 
   $(".LinkFirst").click(function () {
     var elements = GetElements(this);
     var formatted = "[] " + elements.URL + " - " + elements.Title;
-
+    
     GM_setClipboard(formatted);
+    showSnackBar();
+  });
+
+    $(".TitleFirst").click(function () {
+    var elements = GetElements(this);
+    var formatted = "[] " + elements.Title + " - " + elements.URL;
+    
+    GM_setClipboard(formatted);
+    showSnackBar();
   });
 
 })();
@@ -60,17 +64,73 @@ function GetElements(el) {
   return { "Title": title, "URL": url }
 }
 
+function showSnackBar() {
+  var x = document.getElementById("snackbar");
+  // Add class to the snackbar
+  x.className = "show";
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
 GM_addStyle(`
-  .btn-copy-to-clipboard {
-    background-color: #326CE6;
-    border: none;
-    color: white;
-    padding: 5px 8px 5px 8px;
-    margin: 2px 10px 2px 0px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
+ 
+  .btn {
     font-size: 16px;
-    border-radius: 5px;
+    border-radius: 2px;
+    border: 0;
+    transition: .2s ease-out;
+    color: #fff;
+    background: #326CE6;
+    margin: 2px 10px 2px 0px;
+    padding: 5px 8px 5px 8px;
+    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  }
+
+  .btn:hover {
+    color: #fff;
+    background-color: #2b61ce !important;
+    box-shadow: 0 5px 11px 0 rgba(0, 0, 0, 0.18), 0 4px 15px 0 rgba(0, 0, 0, 0.15);
+  }
+
+  /* The snackbar - position it at the bottom and in the middle of the screen */
+  #snackbar {
+    visibility: hidden; 
+    margin-left: -125px; /* Divide value of min-width by 2 */
+    background-color: #333; 
+    color: #fff; 
+    text-align: center; 
+    padding: 8px; 
+    position: fixed; 
+    z-index: 1; 
+    left: 50%; 
+    bottom: 30px; 
+    font-size: 16px;
+  }
+
+  #snackbar.show {
+    visibility: visible; 
+    -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+    animation: fadein 0.5s, fadeout 0.5s 2.5s;
+  }
+
+  /* Animations to fade the snackbar in and out */
+  @-webkit-keyframes fadein {
+    from {bottom: 0; opacity: 0;}
+    to {bottom: 30px; opacity: 1;}
+  }
+
+  @keyframes fadein {
+    from {bottom: 0; opacity: 0;}
+    to {bottom: 30px; opacity: 1;}
+  }
+
+  @-webkit-keyframes fadeout {
+    from {bottom: 30px; opacity: 1;}
+    to {bottom: 0; opacity: 0;}
+  }
+
+  @keyframes fadeout {
+    from {bottom: 30px; opacity: 1;}
+    to {bottom: 0; opacity: 0;}
   }
 `);
