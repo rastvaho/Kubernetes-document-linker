6324ae2e1da1b6b77a7d46f948a2b70ff8e43332
tde908 <tomellis91@gmail.com>
2019-11-13 16:50:04 +1100

    refactor

--- document-linker.js
+++ document-linker.js
@@ -11,126 +11,64 @@
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // ==/UserScript==
 
-var $ = window.jQuery;
 
-(function () {
+(function ($) {
   'use strict';
 
-  $("a.anchorjs-link")
-    .after("<div class=\"buttonDiv\"></div>")
-    
+    var anchorLinks = $("a.anchorjs-link");
+    var absoluteHref;
+    var title;
+    var $this;
+    $.each(anchorLinks, function (){
+        $this = $(this);
+        absoluteHref =$this.prop('href');
+        title = $this.attr('href').replace(/(^#)|(\-)/ig,' ').trim();
+        $this.after("<div>" +
+                    "<button class='js-get-link button button-primary' type='button' data-reference='"+absoluteHref+"' >Plain link</button>" +
+                    "<button class='js-get-link button button-primary' type='button' data-reference='[] "+absoluteHref +" - "+ title +"'>[ ] Link - Title</button>" +
+                    "<button class='js-get-link button button-primary' type='button' data-reference='[] "+title +" - "+ absoluteHref +"'>[ ] Title - Link</button>" +
+                    "<div class='snackbar js-snackbar'>Copied link!</div>" +
+                    "</div>"
+                      );
 
-  $("div.buttonDiv")
-    .append("<button class=\"PlainLink btn btn-primary\" type=\"button\">Plain link</button>")
-    .append("<button class=\"LinkFirst btn btn-primary\" type=\"button\">[ ] Link - Title</button>")
-    .append("<button class=\"TitleFirst btn btn-primary\" type=\"button\">[ ] Title - Link</button>")
-    .append("<div id=\"snackbar\">Copied link!</div>");
+    });
 
-  $(".PlainLink").click(function () {
-    var elements = GetElements(this);
-    
-    GM_setClipboard(elements.URL);
-    showSnackBar();
+  $(".js-get-link").click(function () {
+
+      var $that = $(this);
+      var refrence = $that.data('reference');
+      $that.parent().find('.js-snackbar').addClass('show');
+      setTimeout(function(){  $that.parent().find('.js-snackbar').removeClass('show');; }, 3000);
+      GM_setClipboard(refrence);
+
   });
+})(jQuery);
 
-  $(".LinkFirst").click(function () {
-    var elements = GetElements(this);
-    var formatted = "[] " + elements.URL + " - " + elements.Title;
-    
-    GM_setClipboard(formatted);
-    showSnackBar();
-  });
-
-    $(".TitleFirst").click(function () {
-    var elements = GetElements(this);
-    var formatted = "[] " + elements.Title + " - " + elements.URL;
-    
-    GM_setClipboard(formatted);
-    showSnackBar();
-  });
-
-})();
-
-function GetElements(el) {
-  var anchor = $(el).parents(".buttonDiv").prev();
-  var origin = $(anchor)[0].origin;
-  var path = $(anchor)[0].pathname;
-  var hash = $(anchor)[0].hash;
-  var html = $(el).parents(".buttonDiv").prev().parent()
-  var htmlTitle = $(html)[0].innerHTML
-  var title = htmlTitle.substr(0, htmlTitle.indexOf('<a'));
-  var url = origin + path + hash;
-
-  return { "Title": title, "URL": url }
-}
-
-function showSnackBar() {
-  var x = document.getElementById("snackbar");
-  // Add class to the snackbar
-  x.className = "show";
-  // After 3 seconds, remove the show class from DIV
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-}
 
 GM_addStyle(`
- 
-  .btn {
-    font-size: 16px;
-    border-radius: 2px;
-    border: 0;
-    transition: .2s ease-out;
-    color: #fff;
-    background: #326CE6;
-    margin: 2px 10px 2px 0px;
-    padding: 5px 8px 5px 8px;
-    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
-  }
 
-  .btn:hover {
-    color: #fff;
-    background-color: #2b61ce !important;
-    box-shadow: 0 5px 11px 0 rgba(0, 0, 0, 0.18), 0 4px 15px 0 rgba(0, 0, 0, 0.15);
-  }
+  .button-primary {
+   cursor: pointer;
+   line-height: 24px;
+   margin-right: 8px;
+   padding: 0 10px;
 
+  }
   /* The snackbar - position it at the bottom and in the middle of the screen */
-  #snackbar {
-    visibility: hidden; 
-    margin-left: -125px; /* Divide value of min-width by 2 */
-    background-color: #333; 
-    color: #fff; 
-    text-align: center; 
-    padding: 8px; 
-    position: fixed; 
-    z-index: 1; 
-    left: 50%; 
-    bottom: 30px; 
-    font-size: 16px;
+  .snackbar {
+    visibility: hidden;
+    background-color: #333;
+    border-radius: 19px;
+    color: #fff;
+    display: inline-block;
+    font-size: 0.75rem;
+    padding: 8px;
+    text-align: center;
+    z-index: 1;
   }
 
-  #snackbar.show {
-    visibility: visible; 
-    -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
-    animation: fadein 0.5s, fadeout 0.5s 2.5s;
+  .snackbar.show {
+    visibility: visible;
   }
 
-  /* Animations to fade the snackbar in and out */
-  @-webkit-keyframes fadein {
-    from {bottom: 0; opacity: 0;}
-    to {bottom: 30px; opacity: 1;}
-  }
-
-  @keyframes fadein {
-    from {bottom: 0; opacity: 0;}
-    to {bottom: 30px; opacity: 1;}
-  }
-
-  @-webkit-keyframes fadeout {
-    from {bottom: 30px; opacity: 1;}
-    to {bottom: 0; opacity: 0;}
-  }
-
-  @keyframes fadeout {
-    from {bottom: 30px; opacity: 1;}
-    to {bottom: 0; opacity: 0;}
-  }
 `);
